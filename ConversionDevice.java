ecaa197de0146ccc7c44a96f418a57ef8c84445b
Asmit007 <rapido@Rapidos-MacBook-Pro.local>
2022-07-26 14:32:17 +0530

    Added code for feet to inch conversion

--- ConversionDevice.java
+++ ConversionDevice.java
@@ -1,11 +1,23 @@
 package RapidoTraining;
 
+import java.util.Scanner;
+
 public class ConversionDevice {
 
 
+    static int Convert(int value, int multiplier)
+    {
+        return value*multiplier ;
+    }
+
     public static void main(String []  agrs)
     {
-        System.out.println("HELLO WORLD");
+
+        System.out.println("Enter Feet :");
+        Scanner sc = new Scanner(System.in) ;
+        int feet = sc.nextInt() ;
+
+        System.out.println("In INCH = " + Convert(feet,12) );
     }
 
 
