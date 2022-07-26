712ebe2e8dd572d6ad5829ff0c05a522792b12ab
Asmit007 <rapido@Rapidos-MacBook-Pro.local>
2022-07-26 16:32:13 +0530

    separating code into different files

---
+++ Constants.java
@@ -0,0 +1,8 @@
+package RapidoTraining;
+
+public class Constants {
+    public static final float FEET_TO_INCH_MULTIPLIER = 12 ;
+
+    public static final float YARD_TO_FEET_MULTIPLIER = 3 ;
+
+}
---
+++ utils.java
@@ -0,0 +1,13 @@
+package RapidoTraining;
+public class utils {
+    public static float yardToFeet(float Yards)
+    {
+        return Yards * Constants.YARD_TO_FEET_MULTIPLIER;
+    }
+
+    public static float feetToInch(float Inch)
+    {
+        return Inch * Constants.FEET_TO_INCH_MULTIPLIER ;
+    }
+
+}
