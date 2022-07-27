172ff7554b32f0ed05c3061ac13a9d3b05c31230
Asmit007 <rapido@Rapidos-MacBook-Pro.local>
2022-07-27 11:36:40 +0530

    some changes

--- ConversionDevice.java
+++ ConversionDevice.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ConversionDevice {
     public static void main(String []  agrs)
     {
-        Scanner sc = new Scanner(System.in) ;
+        Scanner scanner = new Scanner(System.in) ;
 
         int inputValue = 1 ;
 
@@ -11,13 +11,13 @@ public class ConversionDevice {
 
             System.out.println("\nCHOOSE CONVERSION SYSTEM : ");
             System.out.println("1.FEET TO INCH\t 2.YARD TO FEET\t  3.INCH TO YARD\t 4.EXIT");
-            inputValue = sc.nextInt() ;
+            inputValue = scanner.nextInt() ;
 
             switch(inputValue){
                 case 1:
                 {
                     System.out.print("Enter Feet : ");
-                    float feetInput = sc.nextFloat() ;
+                    float feetInput = scanner.nextFloat() ;
                     float feetToInch = utils.feetToInch(feetInput) ;
                     System.out.println(feetInput + " feet = " +  feetToInch + " inch" );
                     break ;
@@ -25,7 +25,7 @@ public class ConversionDevice {
 
                 case 2:{
                     System.out.print("Enter Yard : ");
-                    float yardInput = sc.nextFloat() ;
+                    float yardInput = scanner.nextFloat() ;
                     float YardToFeet = utils.yardToFeet(yardInput) ;
                     System.out.println(yardInput + " feet = " + YardToFeet + " yard" );
                     break ;
@@ -33,9 +33,9 @@ public class ConversionDevice {
 
                 case 3:{
                     System.out.print("Enter Inches : ");
-                    float inchInput = sc.nextFloat() ;
+                    float inchInput = scanner.nextFloat() ;
                     float inchToYard = utils.inchToYard(inchInput) ;
-                    System.out.println(inchInput + " feet = " + inchToYard + " yard" );
+                    System.out.println(inchInput + " inch = " + inchToYard + " yard" );
                     break ;
                 }
 
